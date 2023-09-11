2af4541439e49cd05a12c4584602ab5c39f2a897
hongkyun-bae <54279688+hongkyun-bae@users.noreply.github.com>
2023-09-11 15:20:08 +0900

    Add files via upload

---
+++ MIND/model/LSTUR/__init__.py
@@ -0,0 +1,120 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from model.LSTUR.news_encoder import NewsEncoder
+from model.LSTUR.user_encoder import UserEncoder
+from model.general.click_predictor.dot_product import DotProductClickPredictor
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+
+class LSTUR(torch.nn.Module):
+    """
+    LSTUR network.
+    Input 1 + K candidate news and a list of user clicked news, produce the click probability.
+    """
+    def __init__(self, config, pretrained_word_embedding=None):
+        """
+        # ini
+        user embedding: num_filters * 3
+        news encoder: num_filters * 3
+        GRU:
+        input: num_filters * 3
+        hidden: num_filters * 3
+
+        # con
+        user embedding: num_filter * 1.5
+        news encoder: num_filters * 3
+        GRU:
+        input: num_fitlers * 3
+        hidden: num_filter * 1.5
+        """
+        super(LSTUR, self).__init__()
+        self.config = config
+        self.news_encoder = NewsEncoder(config, pretrained_word_embedding)
+        self.user_encoder = UserEncoder(config)
+        self.click_predictor = DotProductClickPredictor()
+        assert int(config.num_filters * 1.5) == config.num_filters * 1.5
+        self.user_embedding = nn.Embedding(
+            config.num_users,
+            config.num_filters * 3 if config.long_short_term_method == 'ini'
+            else int(config.num_filters * 1.5),
+            padding_idx=0)
+
+    def forward(self, user, clicked_news_length, candidate_news, clicked_news):
+        """
+        Args:
+            user: batch_size,
+            clicked_news_length: batch_size,
+            candidate_news:
+                [
+                    {
+                        "category": batch_size,
+                        "subcategory": batch_size,
+                        "title": batch_size * num_words_title
+                    } * (1 + K)
+                ]
+            clicked_news:
+                [
+                    {
+                        "category": batch_size,
+                        "subcategory": batch_size,
+                        "title": batch_size * num_words_title
+                    } * num_clicked_news_a_user
+                ]
+        Returns:
+            click_probability: batch_size
+        """
+        # batch_size, 1 + K, num_filters * 3
+        candidate_news_vector = torch.stack(
+            [self.news_encoder(x) for x in candidate_news], dim=1)
+        # ini: batch_size, num_filters * 3
+        # con: batch_size, num_filters * 1.5
+        # TODO what if not drop
+        user = F.dropout2d(self.user_embedding(
+            user.to(device)).unsqueeze(dim=0),
+                           p=self.config.masking_probability,
+                           training=self.training).squeeze(dim=0)
+        # batch_size, num_clicked_news_a_user, num_filters * 3
+        clicked_news_vector = torch.stack(
+            [self.news_encoder(x) for x in clicked_news], dim=1)
+        # batch_size, num_filters * 3
+        user_vector = self.user_encoder(user, clicked_news_length,
+                                        clicked_news_vector)
+        # batch_size, 1 + K
+        click_probability = self.click_predictor(candidate_news_vector,
+                                                 user_vector)
+        return click_probability
+
+    def get_news_vector(self, news):
+        # batch_size, num_filters * 3
+        return self.news_encoder(news)
+
+    def get_user_vector(self, user, clicked_news_length, clicked_news_vector):
+        """
+        Args:
+            user: batch_size
+            clicked_news_length: batch_size
+            clicked_news_vector: batch_size, num_clicked_news_a_user, num_filters * 3
+        Returns:
+            (shape) batch_size, num_filters * 3
+        """
+        # ini: batch_size, num_filters * 3
+        # con: batch_size, num_filters * 1.5
+        user = self.user_embedding(user.to(device))
+        # batch_size, num_filters * 3
+        return self.user_encoder(user, clicked_news_length,
+                                 clicked_news_vector)
+
+    def get_prediction(self, news_vector, user_vector):
+        """
+        Args:
+            news_vector: candidate_size, word_embedding_dim
+            user_vector: word_embedding_dim
+        Returns:
+            click_probability: candidate_size
+        """
+        # candidate_size
+        return self.click_predictor(
+            news_vector.unsqueeze(dim=0),
+            user_vector.unsqueeze(dim=0)).squeeze(dim=0)
---
+++ MIND/model/LSTUR/news_encoder.py
@@ -0,0 +1,76 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from model.general.attention.additive import AdditiveAttention
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+
+class NewsEncoder(torch.nn.Module):
+    def __init__(self, config, pretrained_word_embedding):
+        super(NewsEncoder, self).__init__()
+        self.config = config
+        if pretrained_word_embedding is None:
+            self.word_embedding = nn.Embedding(config.num_words,
+                                               config.word_embedding_dim,
+                                               padding_idx=0)
+        else:
+            self.word_embedding = nn.Embedding.from_pretrained(
+                pretrained_word_embedding, freeze=False, padding_idx=0)
+        self.category_embedding = nn.Embedding(config.num_categories,
+                                               config.num_filters,
+                                               padding_idx=0)
+        assert config.window_size >= 1 and config.window_size % 2 == 1
+        self.title_CNN = nn.Conv2d(
+            1,
+            config.num_filters,
+            (config.window_size, config.word_embedding_dim),
+            padding=(int((config.window_size - 1) / 2), 0))
+        self.title_attention = AdditiveAttention(config.query_vector_dim,
+                                                 config.num_filters)
+
+    def forward(self, news):
+        """
+        Args:
+            news:
+                {
+                    "category": batch_size,
+                    "subcategory": batch_size,
+                    "title": batch_size * num_words_title
+                }
+        Returns:
+            (shape) batch_size, num_filters * 3
+        """
+        # Part 1: calculate category_vector
+
+        # batch_size, num_filters
+        category_vector = self.category_embedding(news['category'].to(device))
+
+        # Part 2: calculate subcategory_vector
+
+        # batch_size, num_filters
+        subcategory_vector = self.category_embedding(
+            news['subcategory'].to(device))
+
+        # Part 3: calculate weighted_title_vector
+
+        # batch_size, num_words_title, word_embedding_dim
+        title_vector = F.dropout(self.word_embedding(news['title'].to(device)),
+                                 p=self.config.dropout_probability,
+                                 training=self.training)
+        # batch_size, num_filters, num_words_title
+        convoluted_title_vector = self.title_CNN(
+            title_vector.unsqueeze(dim=1)).squeeze(dim=3)
+        # batch_size, num_filters, num_words_title
+        activated_title_vector = F.dropout(F.relu(convoluted_title_vector),
+                                           p=self.config.dropout_probability,
+                                           training=self.training)
+        # batch_size, num_filters
+        weighted_title_vector = self.title_attention(
+            activated_title_vector.transpose(1, 2))
+
+        # batch_size, num_filters * 3
+        news_vector = torch.cat(
+            [category_vector, subcategory_vector, weighted_title_vector],
+            dim=1)
+        return news_vector
